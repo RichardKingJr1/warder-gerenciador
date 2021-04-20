87f1e45651120402ea45fa256e2b3893902317f2
Richard <richard.kingjunior1@gmail.com>
2021-04-20 18:21:49 -0300

    adicionar tracking

---
+++ src/app/Pages/home/services/data-ajuste.service.ts
@@ -0,0 +1,32 @@
+import { NumberFormatStyle } from '@angular/common';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataAjusteService {
+
+  constructor() { }
+
+
+  //ajusta o formato da hora
+  ajustarHora(dia:Date){
+    let h = dia.getHours();
+    let m = dia.getMinutes();
+
+    // add a zero in front of numbers<10
+    let h_string = this.checkTime(h);
+    let m_string = this.checkTime(m);
+
+    return h_string+':'+m_string+':00';
+  }
+
+  //função derivada de ajsutar hora, roda em processamento interno
+  checkTime(i:NumberFormatStyle) {
+    let i_string: string = '';
+    if (i < 10) {
+      i_string = "0" + i;
+    }
+    return i_string;
+  }
+}
